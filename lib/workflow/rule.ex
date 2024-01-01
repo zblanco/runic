78722634fd380da407e587878d79b782a54dfdec
zblanco <zkwhited@gmail.com>
2024-01-01 15:27:13 -0700

    start construction apis

--- lib/workflow/rule.ex
+++ lib/workflow/rule.ex
@@ -1,6 +1,16 @@
 defmodule Runic.Workflow.Rule do
   defstruct name: nil,
             arity: nil,
-            expression: [],
-            workflow: nil
+            workflow: nil,
+            expression: nil
+
+  def new(opts \\ []) do
+    __MODULE__
+    |> struct!(opts)
+    |> Map.put_new(:name, Uniq.UUID.uuid4())
+  end
+
+  # def check(%__MODULE__{} = rule, fact) do
+
+  # end
 end
