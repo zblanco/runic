1ff59849ae5f2fd741a2043f25143379fe6c8852
Zack White <zkwhited@gmail.com>
2026-02-20 16:52:01 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Runic.MixProject do
   def project do
     [
       app: :runic,
-      version: "0.1.0-alpha.1",
+      version: "0.1.0-alpha.2",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
